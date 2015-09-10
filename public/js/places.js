2500772a712178573efb8228dd473158cb41768c
zmateusz <mat309@o2.pl>
2015-09-10 14:02:59 +0200

    display place score/price in table

--- public/js/places.js
+++ public/js/places.js
@@ -9,7 +9,6 @@ function getPlaces() {
     // data: { page: 1 },
     statusCode: {
       200: function(data) {
-        dd = data;
         var tableBody = $("table tbody");
         for (var i=0; i < data.places.length; i++) {
           newTableRow(data.places[i], tableBody);
@@ -40,12 +39,16 @@ function newTableRow(place, tableBody) {
   clone.find(".media-description").text(place.description);
   var icons = clone.find(".media-icons");
   if (place.wifi) icons.append('<span class="icon-connection">');
-  if (place.tv) icons.append('<span class=" icon-tv">');
+  if (place.tv) icons.append('<span class="icon-tv">');
   if (place.food) icons.append('<span class="icon-spoon-knife">');
   if (place.paypas) icons.append('<span class="icon-credit-card">');
-  if (place.swimming) icons.append('<span class=" icon-lifebuoy">');
-  if (place.airport) icons.append('<span class=" icon-airplane">');
+  if (place.swimming) icons.append('<span class="icon-lifebuoy">');
+  if (place.airport) icons.append('<span class="icon-airplane">');
   if (place.parking) icons.append('<span><strong>P</strong></span');
+  clone.find(".score").html("Ocena "+place.score+"/<small>10</small>");
+  clone.find(".opinions").text("Ocena na podstawie "+place.opinion_count+" opinii");
+  clone.find(".oldprice").html("<s>"+place.oldprice+",00PLN</s>");
+  clone.find(".price").prepend(place.price);
   tableRow.append(clone);
   tableBody.append(tableRow);
 };
