8cf2ee6bc37bf94f30a98d01af53951a1415a9d2
zmateusz <mat309@o2.pl>
2015-09-11 23:42:32 +0200

    script for video control (player API)

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,32 @@
+$(function() {
+  var player = $('iframe');
+  var playerOrigin = '*';
+
+  // Helper function for sending a message to the player
+  // works only from web server - not locally
+  // https://developer.vimeo.com/player/js-api
+  function post(action, value) {
+    var data = {
+      method: action
+    };
+    
+    if (value) {
+      data.value = value;
+    };
+
+    var message = JSON.stringify(data);
+    player[0].contentWindow.postMessage(data, playerOrigin);
+  };
+
+  $('#myModal').on('shown.bs.modal', function (e) {
+    post("play");
+  });
+  $('#myModal').on('hidden.bs.modal', function (e) {
+    post("pause");
+  });
+
+});
+
 $("#btnSend").click(function(event) {
   event.preventDefault();
 
